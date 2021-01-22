d9d919bc64802ee8899192479755ef07f7e319d6
Milad Gholami <milad621@gmail.com>
2021-01-22 15:15:36 -0800

    fix op

--- hw2/single_file.py
+++ hw2/single_file.py
@@ -321,16 +321,16 @@ class Parser(object):
         node = self.aexp()
         if self.current_token.type == '=':
             self.eat('=')
-            return BinOp(left=node, op='=', right=self.aexp())
+            return BinOp(left=node, op=Token('=', '='), right=self.aexp())
         if self.current_token.type == '<':
             self.eat('<')
-            return BinOp(left=node, op='<', right=self.aexp())
+            return BinOp(left=node, op=Token('<','<'), right=self.aexp())
 
     def b_and(self):
         node = self.bcmpr()
         while self.current_token.type == '∧':
             self.eat('∧')
-            node = BinOp(left=node, op='∧', right=self.bcmpr())
+            node = BinOp(left=node, op=Token('∧','∧'), right=self.bcmpr())
         return node
         
     def b_or(self):
@@ -343,7 +343,7 @@ class Parser(object):
         node = self.b_and()
         if self.current_token.type == '∨':
             self.eat('∨')
-            node = BinOp(left=node, op='∨', right=self.b_and())
+            node = BinOp(left=node, op=Token('∨','∨'), right=self.b_and())
         return node
 
     def command(self):
@@ -369,10 +369,17 @@ class Parser(object):
             c2 = self.comma_command()
             return If(b, c1, c2)
         if self.current_token.type == 'while':
+            # print("current token:", self.current_token.type, self.current_token.value)
             self.eat('while')
+            # print("current token:", self.current_token.type, self.current_token.value)
             b = self.b_or()
+            # print(b, b.token, b.value)
+            # print("current token:", self.current_token.type, self.current_token.value)
             self.eat('do')
+            # print("current token:", self.current_token.type, self.current_token.value)
             c = self.comma_command()
+            # print(c)
+            # print("current token:", self.current_token.type, self.current_token.value)
             return While(b, c)
         if self.current_token.type == VAR:
             x = Var(self.current_token.value)
